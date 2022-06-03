086bed9a618aa32c27bcfe9b5780b8ee0f2c6e77
wo-shi-nibaba <75060591+wo-shi-nibaba@users.noreply.github.com>
2022-06-03 23:55:09 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+import re
 import rarfile
 import zipfile
 import tarfile
@@ -8,10 +9,44 @@ from werkzeug.utils import secure_filename
 from scannerPackages import Scanner
 from pathlib import Path
 import parse_CVE
+from Utils import package_license,license_detail
+
+def isver(ver):
+    if type(ver)!=type(''):
+        return False
+    elif len(ver)==0:
+        return False
+    else:
+        return ver[0]<='9' and ver[0]>='0'
+
+class IgnoreCaseDict(dict):
+    
+    def lower_key(self, key):
+        if isinstance(key, str):
+            return key.lower()
+        return key
+
+    def __setitem__(self, key, value):
+        super().__setitem__(self.lower_key(key), value)
+
+    def __getitem__(self, item):
+        return super().__getitem__(self.lower_key(item))
+
+    def __delitem__(self, key):
+        super().__delitem__(self.lower_key(key))
+
+    def update(self, another=None, **F):
+        for key, value in another.items():
+            self.__setitem__(key, value)
+
+    def __repr__(self):
+        return f"{type(self).__name__}({super().__repr__()})"
+    
 base='C:/Users/Administrator.DESKTOP-B75U3IK/Desktop/作品赛/'
 save_route='C:/Users/Administrator.DESKTOP-B75U3IK/Desktop/作品赛/saver/'
 app = Flask(__name__)
-versions=dict()
+versions=IgnoreCaseDict()
+license_dict=dict()
 def unrar(path,route):
     notgz=True
     if path.endswith('.rar'):
@@ -42,8 +77,12 @@ def prosrar(full_path,save_route):
                 pyfiles.append(filepath)
             elif name=="requirements.txt":
                 filepath=os.path.join(root,name)
-                with open(filepath,'r',encoding='utf-16')as f:
-                    vers=f.readlines()
+                try:
+                    with open(filepath,'r',encoding='utf-8')as f:
+                        vers=f.readlines()
+                except:
+                    with open(filepath,'r',encoding='utf-16')as f:
+                        vers=f.readlines()
                 global versions
                 for each in vers:
                     loc=each.find('==')
@@ -82,8 +121,35 @@ def uploader():
                 contents=[g.read()]
         global packages
         packages=list()
+        global license_dict
+        license_dict=dict()
         for i in contents:
-            packages.append((i,versions.get(i)))
+            pack=i
+            ver=versions.get(pack)
+            if ver==None:
+                if i=='bs4':
+                    ver=versions.get("beautifulsoup4")
+                    pack='beautifulsoup4'
+                elif i=='PIL':
+                    ver=versions.get("pillow")
+                    if ver!=None:
+                        pack='pillow'
+                elif i=='cv2':
+                    ver=versions.get("opencv_python")
+                    pack='opencv'
+            tmp_dict={'name':None,'version':None,'license_expression':'','licenses_summary':'[]'}
+            t1=package_license([(pack,ver)])
+            if len(t1):
+                t2=t1[0]
+                if len(t2):
+                    tmp_dict=t2[0]
+            package_lic=tmp_dict
+            lic_ver=package_lic['version']
+            if ver==None and isver(lic_ver):
+                ver=lic_ver
+            license_dict[pack]=package_lic
+            versions[pack]=ver
+            packages.append((pack,ver))
         return redirect(url_for('result',file=fname))
     
 @app.route('/result',methods=['GET'])
@@ -97,14 +163,22 @@ def result():
 @app.route('/licenses',methods=['GET'])
 def licenses():
     file=request.args.get("file")
-    return render_template("result.html",contents=packages,file=file)
+    return render_template("licenses.html")
 
 @app.route('/license',methods=['GET'])
 def license():
     file=request.args.get("file")
     pack=request.args.get('pack')
     ver=versions.get(pack)
-    return render_template("result.html",contents=[(pack,ver)],file=file)
+    checklst=[]
+    flag_for_license=(license_dict[pack]['license_expression']!='')
+    license_summary=eval(license_dict[pack]['licenses_summary'])
+    length=len(license_summary)
+    counter=list(range(length))
+    for diction in license_summary:
+        checklst.append(diction['key'])
+    detail=license_detail(checklst)
+    return render_template("license.html",pack=pack,ver=ver,counter=counter,flag=flag_for_license,detail=detail,summary=license_summary,file=file)
 
 @app.route('/cve',methods=['GET'])
 def cve():
@@ -113,13 +187,21 @@ def cve():
     ver=versions.get(pack)
     packs=[(pack,ver)]
     cve_dict=parse_CVE.parse_CVE(packs)
-    return render_template("cves.html",packs=packs,holes=cve_dict,file=file)
+    has_vul=dict()
+    for pack in packs:
+        if(len(cve_dict[pack[0]])):
+            has_vul[pack[0]]=True
+    return render_template("cves.html",packs=packs,has_vul=has_vul,holes=cve_dict,file=file)
 
 @app.route('/cves',methods=['GET'])
 def cves():
     file=request.args.get("file")
     cve_dict = parse_CVE.parse_CVE(packages)
-    return render_template("cves.html",packs=packages,holes=cve_dict,file=file)
+    has_vul=dict()
+    for pack in packages:
+        if(len(cve_dict[pack[0]])):
+            has_vul[pack[0]]=True
+    return render_template("cves.html",packs=packages,has_vul=has_vul,holes=cve_dict,file=file)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5678)
